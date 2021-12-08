907a414bbb00d07849c7c72b94236d795d02f140
Manas-0908 <manas.jha1991@gmail.com>
2021-12-08 18:50:22 +0530

    into to node

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroe =require("superheroes");
+var supervillain =require("supervillains");
+
+var mySuperHeroName=superheroe.random();
+var mySupervillainName=supervillain.random();
+console.log(mySuperHeroName);
+console.log(mySupervillainName);
